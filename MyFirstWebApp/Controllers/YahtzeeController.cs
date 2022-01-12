4c62ea6b82896284f6181b97f16815bd2064b208
Csbennett22 <90526059+Csbennett22@users.noreply.github.com>
2022-01-12 13:06:47 -0700

    Full Web App

---
+++ MyFirstWebApp/Controllers/YahtzeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class YahtzeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //throw new IndexOutOfRangeException();
+            return View();
+        }
+
+        public IActionResult Gradeform ()
+        {
+            return View();
+        }
+    }
+}
